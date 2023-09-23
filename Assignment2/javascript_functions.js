4b333a633338be32effaf1a7612811fe0f376ab1
BrajanHalili <halilibrajan@gmail.com>
2023-09-23 16:18:57 -0400

    Updated Javascript_functions
    Added mySome, myEvery and myReduce to the javascript file.

--- Assignment2/javascript_functions.js
+++ Assignment2/javascript_functions.js
@@ -1,1 +1,28 @@
+const mySome = (callbackfn, array)=>{
+    for(let i =0; i<array.length; i++){
+        if(callbackfn(array[i],i,array)){
+            return true;
+        }
+    }
+    return false;
+}
 
+const myEvery = (callbackfn, array)=>{
+    for(let i = 0; i<array.length;i++){
+        if(callbackfn(array[i],i,array) === false){
+            return false;
+        }
+    }
+    return true;
+}
+
+const myReduce = (callbackfn, array, initialVal)=>{
+    let num = 0;
+    for(let i = 0; i<array.length;i++){
+        num = callbackfn(num,array[i],i,array);
+    }
+    if(initialVal != undefined){
+        return num + initialVal;
+    }
+    else return num;
+}
