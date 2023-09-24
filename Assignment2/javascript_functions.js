91050b5e26559c464b8035085f155d9f1e756f63
Utrejo1125 <u.trejo1125@gmail.com>
2023-09-24 14:41:37 -0400

    Added three JS functions

--- Assignment2/javascript_functions.js
+++ Assignment2/javascript_functions.js
@@ -1,1 +1,22 @@
+//takes in an array of eleents and indicate wheter or not a target element is contained in input array
+const myIncludes = (array, target)=>{
+    for(let i = 0; i < array.length; i++)
+        if (array[i] === target)
+            return true;
 
+    return false;
+}
+
+//takes in an array of elements and returns the index of the first encounter of a target element (if it is found)
+const myIndexOf = (array, target)=>{
+    for(let i = 0; i < array.length; i++)
+        if (array[i] === target)
+            return i;
+    
+    return -1;
+}
+
+//takes in an array of elements as well as an elementToAdd and append that element to the end of the array.
+const myPush = (array, elementtoAdd)=>{
+    array[array.length] = elementtoAdd;
+}
