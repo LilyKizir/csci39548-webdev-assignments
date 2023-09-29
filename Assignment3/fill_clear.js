ba9bbe1ac3cac53d9dc00210129efe3c82e8c6c6
Utrejo1125 <u.trejo1125@gmail.com>
2023-09-29 12:14:42 -0400

    Merged Fill Uncolored and fixed associated code

--- Assignment3/fill_clear.js
+++ Assignment3/fill_clear.js
@@ -26,9 +26,12 @@ var fillUncolored = document.getElementById("fillUncolored");
 fillUncolored.addEventListener("click", () => {
     var cell_color = document.getElementById("colorPicker").value;
     allCells = document.querySelectorAll('td');
+    console.log(allCells.length);
     allCells.forEach((cell) => {
         var cellBackgroundColor = window.getComputedStyle(cell).backgroundColor;
+        console.log(cellBackgroundColor);
         if (cellBackgroundColor === "rgb(255, 255, 255)") {
             cell.style.backgroundColor = cell_color;
         }
+    });
 });
