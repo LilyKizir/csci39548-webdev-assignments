90f5d41423fbd7ae444b174c699a11deeef3f6c7
Utrejo1125 <u.trejo1125@gmail.com>
2023-09-29 12:24:39 -0400

    Changed code to check for clear instead of white

--- Assignment3/fill_clear.js
+++ Assignment3/fill_clear.js
@@ -17,7 +17,7 @@ var clearbtn = document.getElementById("clearbtn");
 clearbtn.addEventListener("click", () => {
     allCells = document.querySelectorAll('td');
     allCells.forEach((cell) => {
-        cell.style.backgroundColor = "white";
+        cell.style.backgroundColor = "";
     });
 });
 
@@ -26,11 +26,11 @@ var fillUncolored = document.getElementById("fillUncolored");
 fillUncolored.addEventListener("click", () => {
     var cell_color = document.getElementById("colorPicker").value;
     allCells = document.querySelectorAll('td');
-    console.log(allCells.length);
+    //console.log(allCells.length);
     allCells.forEach((cell) => {
         var cellBackgroundColor = window.getComputedStyle(cell).backgroundColor;
         console.log(cellBackgroundColor);
-        if (cellBackgroundColor === "rgb(255, 255, 255)") {
+        if (cellBackgroundColor === "rgba(0, 0, 0, 0)") {
             cell.style.backgroundColor = cell_color;
         }
     });
