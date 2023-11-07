0918cc9c66a9e968bea07c58e4b2aa4b9a7eeeec
Utrejo1125 <u.trejo1125@gmail.com>
2023-11-06 19:12:34 -0500

    Added a random photo call with a query option

--- Assignment5/src/App.js
+++ Assignment5/src/App.js
@@ -1,3 +1,40 @@
-export default function Square() {
-  return <button className="square">X</button>;
+import { useState } from "react";
+
+export default function Gallery() {
+  const [pictureData, setPictureData] = useState({ title: null, url: null, explanation: null });
+  const [query, setQuery] = useState("food");
+
+  const endpoint = 'https://api.unsplash.com/photos/random?client_id=IZmoCbCGKKTr4fDIWDH1SObu3dr3PJaG4h6aeJfO75I&query=';
+
+  function handleQueryChange(e) {
+    setQuery(e.target.value);
+  }
+  
+  function handleGetPictureClick() {
+    fetch(endpoint+query)
+        .then(response => response.json())
+        .then(json => {
+            setPictureData({
+                title: json.alt_description,
+                url: json.urls.regular,
+                explanation: json.description
+            });
+        })
+        .catch(error => console.error(error));
+  }
+
+  return (
+    <>
+      <h2>Random Photo</h2>
+      <label>
+        Generate Picture with Query:
+        <input onChange={handleQueryChange} value={query} />
+      </label>
+      <button onClick={handleGetPictureClick}>Get Picture</button>
+
+      <img src={pictureData.url} alt="random-pic" />
+      <h2>{pictureData.title}</h2>
+      <p>{pictureData.explanation}</p>
+    </>
+  );
 }
