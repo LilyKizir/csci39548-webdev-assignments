3a78e2e6da917424b0785cc3481e3ad3eab87131
BrajanHalili <halilibrajan@gmail.com>
2023-11-06 21:32:47 -0500

    A5:Random Image button
    Adds random image button that shows a random image. Also retrieves the username for photo credit.

--- Assignment5/src/App.js
+++ Assignment5/src/App.js
@@ -1,15 +1,28 @@
 import { useState } from "react";
 
 export default function Gallery() {
-  const [pictureData, setPictureData] = useState({ title: null, url: null, explanation: null });
+  const [pictureData, setPictureData] = useState({ title: null, url: null, explanation: null, username: null });
   const [query, setQuery] = useState("food");
 
   const endpoint = 'https://api.unsplash.com/photos/random?client_id=IZmoCbCGKKTr4fDIWDH1SObu3dr3PJaG4h6aeJfO75I&query=';
 
+  function randomPhoto(){
+    fetch('https://api.unsplash.com/photos/random?client_id=7B3CH3BWU0rb1zCMN4-CEVShZygiksQztkwJPa0R7mo')
+        .then(response => response.json())
+        .then(json => {
+          setPictureData({
+            title: json.alt_description,
+            url: json.urls.regular,
+            explanation: json.description,
+            username: "Picture by: " + json.user.name
+        });
+        })
+  }
+
   function handleQueryChange(e) {
     setQuery(e.target.value);
   }
-  
+
   function handleGetPictureClick() {
     fetch(endpoint+query)
         .then(response => response.json())
@@ -17,7 +30,8 @@ export default function Gallery() {
             setPictureData({
                 title: json.alt_description,
                 url: json.urls.regular,
-                explanation: json.description
+                explanation: json.description,
+                username: "Picture by: " + json.user.name
             });
         })
         .catch(error => console.error(error));
@@ -25,16 +39,20 @@ export default function Gallery() {
 
   return (
     <>
-      <h2>Random Photo</h2>
+      <h2>Get Random Photo</h2>
+      <button onClick={randomPhoto}> Generate</button>
+
+      <h2>Random Photo with Query</h2>
       <label>
         Generate Picture with Query:
         <input onChange={handleQueryChange} value={query} />
       </label>
       <button onClick={handleGetPictureClick}>Get Picture</button>
-
-      <img src={pictureData.url} alt="random-pic" />
+      <br></br>
+      <img src={pictureData.url} alt={pictureData.explanation} />
       <h2>{pictureData.title}</h2>
       <p>{pictureData.explanation}</p>
+      <p>{pictureData.username}</p>
     </>
   );
 }
