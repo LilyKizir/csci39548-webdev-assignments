26c26f2fc68168cb759624a45ecd9c7753985622
BrajanHalili <halilibrajan@gmail.com>
2023-11-19 22:11:02 -0500

    A6Routing
    Created the framework and the routing for the Random and Query pages.

---
+++ Assignment 6/src/main.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+//import Gallery  from './App';
+import Random from './Random';
+import Root from "./routes/root";
+import Query from './Query';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+  },
+  {
+    path: "/Random",
+    element: <Random/>,
+  },
+  {
+    path: "/Query",
+    element: <Query/>,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
