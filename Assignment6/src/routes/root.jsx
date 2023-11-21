a971a237f63193374a78cc83ac3ee2c4371b4867
Lily Kiziriya <107429746+lk20918n@users.noreply.github.com>
2023-11-21 16:38:48 -0500

    a6 - styled for clarity and basic root html

--- Assignment6/src/routes/root.jsx
+++ Assignment6/src/routes/root.jsx
@@ -1,20 +1,49 @@
-import { Link } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 
 export default function Root() {
   return (
     <>
-      <h1>Home Page</h1>
-      <br />
-      <ul>
-        <li>
-          {/* Endpoint to route to About component */}
-          <Link to="/random">Click to go to the Random page</Link>
-        </li>
-        <li>
-          {/* Endpoint to route to Home component */}
-          <Link to="/query">Click to go to the Query Page</Link>
-        </li>
-      </ul>
+      <div id="sidebar">
+        <h1>Dizzy Dev Unsplash Gallery</h1>
+        <nav>
+          <ul>
+            <li>
+              <a href={`/contacts/1`}>Stats go here</a>
+            </li>
+            <li>
+              <a href={`/contacts/2`}>Author Data goes here</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      {/*  */}
+      {/*  */}
+      {/*  */}
+      <div class="main">
+        <div class="search">
+          <form id="search-form" role="search">
+            <input
+              id="q"
+              aria-label="Search contacts"
+              placeholder="Search"
+              type="search"
+              name="q"
+            />
+            <div id="search-spinner" aria-hidden hidden={true} />
+            <div className="sr-only" aria-live="polite"></div>
+          </form>
+          <Link to="/query">
+            <button type="submit">Query</button>
+          </Link>
+          <Link to="/random">
+            {/* <button onClick={randomPhoto}> Random</button> */}
+            <button type="submit">Random</button>
+          </Link>
+        </div>
+        <div id="detail">
+          <Outlet />
+        </div>
+      </div>
     </>
   );
 }
