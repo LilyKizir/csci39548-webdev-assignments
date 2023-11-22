7f787a1fbe2391584a6e42e6c24ff63cc844a5ca
Lily Kiziriya <107429746+lk20918n@users.noreply.github.com>
2023-11-21 19:58:59 -0500

    Update RandomPictureDataSlice.jsx

--- Assignment6/src/Random page/RandomPictureDataSlice.jsx
+++ Assignment6/src/Random page/RandomPictureDataSlice.jsx
@@ -1,23 +1,27 @@
 import { createSlice } from '@reduxjs/toolkit'
 
 export const PictureDataSlice = createSlice({
-    name: 'pictureData',
-    initialState: {
-      title: null,
-      url: null,
-      explanation: null,
-      username: null,
-      userLink: null,
-    },
-    reducers: {
-      setPicture: (state, action) => {
-        state.title = action.payload.alt_description,
-        state.url = action.payload.urls.regular,
-        state.explanation = action.payload.description,
-        state.username = "Picture by: " + action.payload.user.name,
-        state.userLink = action.payload.user.links.html
-    },
+  name: 'pictureData',
+  initialState: {
+    title: null,
+    url: null,
+    explanation: null,
+    displayname: null,
+    username: null,
+    userLink: null,
+    id: null,
   },
+  reducers: {
+    setPicture: (state, action) => {
+      state.title = action.payload.alt_description,
+      state.url = action.payload.urls.regular,
+      state.explanation = action.payload.description,
+      state.displayname = "Picture by: " + action.payload.user.name,
+      state.username = action.payload.user.username,
+      state.userLink = action.payload.user.links.html
+      state.id = action.payload.id
+  },
+},
 })
 
 export const { setPicture } = PictureDataSlice.actions
@@ -25,8 +29,11 @@ export const selectPictureData = (state) => {
   return {
     title : state.pictureData.title, 
     url: state.pictureData.url, 
-    explanation: state.pictureData.explanation, 
+    explanation: state.pictureData.explanation,
+    displayname: state.displayname, 
     username: state.pictureData.username,
-    userLink: state.pictureData.userLink}
+    userLink: state.pictureData.userLink,
+    id: state.pictureData.id
+  }
 } 
 export default PictureDataSlice.reducer
