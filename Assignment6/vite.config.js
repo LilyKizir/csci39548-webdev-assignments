a4ba9ee02370f89241d250a2048e461575755562
BrajanHalili <halilibrajan@gmail.com>
2023-11-21 22:47:44 -0500

    Update vite config

--- Assignment6/vite.config.js
+++ Assignment6/vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "https://lk20918n.github.io/csci39548-webdev-assignments/Assignment6/build",
   plugins: [react()],
 })
